eb96b21b51515a7413b034981f2668a21134f732
idonacmias <idonacmi@gmail.com>
2022-12-26 20:57:18 +0200

    remove secret key from setting

--- dnd/dnd/settings.py
+++ dnd/dnd/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nv)eh+8-#l^pr=p=dq8ey)tuf9w25&(#xk&s+ddcb1ma!05%*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
