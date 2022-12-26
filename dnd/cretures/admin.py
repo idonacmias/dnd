1e6f1401ef2bb1cf2d136006ae2538634726fcb2
idonacmias <idonacmi@gmail.com>
2022-12-27 00:17:35 +0200

    add to cretures model Player , Adventurer, Skills_proficiency. & update to_list.txt

--- dnd/cretures/admin.py
+++ dnd/cretures/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Cretures
+from .models import Creture, Adventurer, Player, Skills_proficiency
 # Register your models here.
 
 
-admin.site.register(Cretures)
+admin.site.register(Creture)
+admin.site.register(Adventurer)
+admin.site.register(Player)
+admin.site.register(Skills_proficiency)
--- dnd/cretures/models.py
+++ dnd/cretures/models.py
@@ -2,19 +2,57 @@ from django.db import models
 
 # Create your models here.
 
-class Cretures(models.Model):
+class Player(models.Model):
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	
+	adventurer = models.ForeignKey("Adventurer", on_delete=models.CASCADE)
+
+	class Meta:
+		unique_together = [['first_name', 'last_name']]
+
+	def __str__(self):
+		return f'{self.first_name} {self.last_name}'
+
+class Creture(models.Model):
 	name = models.CharField(max_length=45)
 	#attributes
 	Strength = models.IntegerField()
-	Constitution = models.IntegerField()
 	Dexterity = models.IntegerField()
+	Constitution = models.IntegerField()
 	Intelligence = models.IntegerField()
 	Wisdom = models.IntegerField()
 	Charisma = models.IntegerField()
-	Charisma = models.IntegerField()
-	proficiency =  models.IntegerField()
 	
-
+	hp = models.IntegerField()
 
 	def __str__(self):
 		return f'{self.name}'
+
+
+class Adventurer(models.Model):
+
+	creture = models.OneToOneField(Creture, on_delete=models.CASCADE)
+
+	level = models.IntegerField()
+	profession = models.CharField(max_length=25)
+	races = models.CharField(max_length=25)
+	skills_proficiency = models.ForeignKey("Skills_proficiency", on_delete=models.CASCADE)
+
+
+
+	def __str__(self):
+		return f'{self.creture.name}'
+
+
+class Skills_proficiency(models.Model):
+	NAME = [('one'), ('two')]
+	name = models.CharField(max_length=25)
+	value = models.IntegerField()
+
+	def __str__(self):
+		return f'{self.name}: {self.value}'
+
+
+
+
